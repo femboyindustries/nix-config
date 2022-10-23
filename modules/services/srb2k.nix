d83fc92136a16db98807991b8e964a86503a9183
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2022-10-23 11:57:12 +0200

    srb2k: every fiber of my being is now declaratively defined

--- modules/services/srb2k.nix
+++ modules/services/srb2k.nix
@@ -12,6 +12,7 @@ let
     "+advertise ${toString (if cfg.advertise then 1 else 0)}"
     "-port ${toString cfg.port}"
     "-serverport ${toString cfg.port}"
+    "-home ${cfg.dataDir}"
   ] ++ cfg.flags;
 in {
   options.modules.services.srb2k = {
@@ -28,7 +29,11 @@ in {
     dataDir = mkOption {
       type = types.str or types.path;
       default = "/var/lib/srb2k";
-      description = "The directory where srb2k will store addons and state information";
+      description = ''
+        The directory where SRB2K will store state information.
+        Due to limitations with the game itself, the game will
+        store state information in a subfolder named '.srb2kart'.
+      '';
     };
 
     port = mkOption {
@@ -40,7 +45,7 @@ in {
     addons = mkOption {
       type = types.listOf (types.str or types.path);
       default = [];
-      description = "Locations of srb2k addons and also fungus spore tasty in your body they grow happy you grow happy";
+      description = "Locations of SRB2K addons and also fungus spore tasty in your body they grow happy you grow happy";
     };
 
     advertise = mkOption {
@@ -65,6 +70,38 @@ in {
       type = types.bool;
       default = true;
       description = "Automatically open ports in the firewall.";
+    };
+
+    config = mkOption {
+      type = with types; attrsOf (oneOf [ bool int str ]);
+      default = {};
+      example = literalExpression ''
+        {
+          maxplayers = 16;
+          http_source = "https://yugoslavia.best/srb2kaddons/";
+          maxsend = "max";
+          servername = with colors; "[EU] ''${lime}yugoslavia.best";
+          server_contact = "oat.zone";
+        }
+      ''; #"
+      description = "Options to put into dkartconfig.cfg";
+    };
+
+    serv = mkOption {
+      type = types.str;
+      default = "";
+      description = "Additional commands to run on startup via kartserv.cfg";
+      example = literalExpression ''
+        with colors; '''
+          fr_enabled off
+          khaosenable off
+          
+          hm_motd on
+          hm_motd_nag on
+          hm_motd_name "''${lime}yugoslavia.best"
+          hm_motd_tagline "home of bar"
+        '''
+      '';
     };
   };
 
@@ -98,6 +135,20 @@ in {
 
         ${tmux} -S ${tmuxSock} send-keys quit Enter
       '';
+
+      kartConfig =
+        let
+          cfgToString = v: if builtins.isBool v then boolToString v else toString v;
+        in
+        pkgs.writeText "kartconfig.cfg" (
+          ''
+            // SRB2Kart configuration file.
+            // Managed by NixOS configuration
+          ''
+          + concatStringsSep "\n" (mapAttrsToList (n: v: "${n} \"${cfgToString v}\"") cfg.config)
+        );
+
+      kartServ = pkgs.writeText "kartserv.cfg" cfg.serv;
     in {
       description = "srb2k server =)";
       wantedBy = mkIf cfg.autoStart [ "multi-user.target" ];
@@ -114,8 +165,13 @@ in {
       };
 
       preStart = ''
-        ${pkgs.coreutils}/bin/chown srb2k:srb2k ${cfg.dataDir}
-        ${pkgs.coreutils}/bin/chmod -R 775 ${cfg.dataDir}
+        umask u=rwx,g=rwx,o=rx
+        mkdir -p ${cfg.dataDir}/.srb2kart
+        cd ${cfg.dataDir}
+        ${if cfg.config != {} then "ln -sf ${kartConfig} .srb2kart/dkartconfig.cfg" else ""}
+        ${if cfg.serv != "" then "ln -sf ${kartServ} .srb2kart/kartserv.cfg" else ""}
+        ${pkgs.coreutils}/bin/chown srb2k:srb2k .
+        ${pkgs.coreutils}/bin/chmod -R 775 .
       '';
 
       postStart = ''
