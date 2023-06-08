9d93277716663348b6486e8b9df29e66d700f532
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2023-06-08 20:13:08 +0200

    mess with nitter (and fail)

--- overlays/nitter.nix
+++ overlays/nitter.nix
@@ -1,12 +1,12 @@
 self: super: {
   nitter = super.nitter.overrideAttrs (old: {
     # https://github.com/zedeus/nitter/pull/830
-    version = "unstable-2023-04-16";
+    version = "unstable-2023-05-31";
     src = super.fetchFromGitHub {
-      owner = "PrivacyDevel";
+      owner = "zedeus";
       repo = "nitter";
-      rev = "11279e2b4ff612f523380c2ff4678a056eb5c03c";
-      hash = "sha256-GSBtyrrQTYRO9+XNXZsXOtnQ5QrLqmKE81RkuX/btUs=";
+      rev = "38985af6ed30f050201b15425cdac0dc2e286b6d";
+      hash = "sha256-YPwApMCsra/T5EzCup28/4FaOrhEuw3MBiitv+LTbi0=";
     };
   });
 }
