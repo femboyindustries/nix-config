882b62fa8bed7b7d578c54ab8e63dd189a3a83e6
Jade Monoids <oatmealine@disroot.org>
2026-06-22 19:34:32 +0200

    update hardware config in accordance to The Server

--- default.nix
+++ default.nix
@@ -6,13 +6,6 @@ in {
   imports =
     _.mapModulesRec' ./modules import;
 
-  # ‹nix flake check› bypasses, can be changed in the actual hosts
-  # config, useful for testing
-  fileSystems."/".device = lib.mkDefault "/dev/disk/by-label/nixos";
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
-
   nix = let
     registry = lib.mapAttrs (name: value: { flake = value; }) (filterAttrs (name: value: name != "attrs") inputs);
   in {
--- hosts/nanachi/hardware-configuration.nix
+++ hosts/nanachi/hardware-configuration.nix
@@ -1,45 +1,11 @@
-{ config, lib, pkgs, inputs, modulesPath, ... }:
-
+{ pkgs, modulesPath, ... }:
 {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
-  boot = {
-    initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
-    initrd.kernelModules = [ ];
-    kernelPackages = pkgs.linuxPackages_hardened;
-    kernelModules = [ "kvm-amd" ];
-    loader = {
-      # and them squiggles steady shifting in the wind
-      grub.enable = lib.mkForce false;
-
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-  };
-
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-uuid/2fb43a32-d7c2-4ed1-97c6-4588d731a132";
-      fsType = "xfs";
-      options = [
-        "noatime"
-        "nodiratime"
-        "discard"
-      ];
-    };
-
-    "/boot" = {
-      device = "/dev/disk/by-uuid/7192-FE7C";
-      fsType = "vfat";
-    };
-  };
-
-  swapDevices = [ ];
-
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.loader.grub.device = "/dev/sda";
+  boot.kernelPackages = pkgs.linuxPackages_hardened;
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  powerManagement.cpuFreqGovernor = "performance";
+  nixpkgs.hostPlatform = "x86_64-linux";
 }
