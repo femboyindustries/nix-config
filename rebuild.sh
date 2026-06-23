8d3fe4a52288cb67407f83562fdc85f0a82b0890
Jade "oatmealine" Monoids <oatmealine@disroot.org>
2026-06-23 23:50:49 +0300

    fix rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
-doas chmod o+x /srv/minecraft -R # god help me
 deriv=$(NIXPKGS_ALLOW_UNFREE=1 nix build -v --no-link --print-out-paths path:.#nixosConfigurations."$(hostname)".config.system.build.toplevel --impure)
 doas nix-env -p /nix/var/nix/profiles/system --set $deriv
 doas $deriv/bin/switch-to-configuration switch
